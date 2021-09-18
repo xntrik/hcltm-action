cbf38fa2195366230e5df6d7028bae81fa5e97e1
xntrik <xntrik@gmail.com>
2021-09-18 11:17:30 +0800

    Action now supports dashboard command - I think

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,8 @@ if [ $1 = "validate" ]
 then
   echo ">>>> Running validate"
   bash -c "set -e;  set -o pipefail; hcltm validate $2"
+elif [ $1 = "dashboard" ]
+then
+  echo ">>>> Running dashboard"
+  bash -c "set -e; set -o pipefail; hcltm dashboard -overwrite -outdir=$3 $2"
 fi
