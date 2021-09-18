bd25b586dbc97601ec077094519063e21fd64a7b
xntrik <xntrik@gmail.com>
2021-09-18 10:59:53 +0800

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
-echo ">>> Running command"
+echo ">>> Starting hcltm-action"
 echo ""
 if [ $1 = "validate" ]
 then
+  echo ">>>> Running validate"
   bash -c "set -e;  set -o pipefail; hcltm validate $2"
 fi
